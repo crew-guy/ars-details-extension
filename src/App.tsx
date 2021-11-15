3be6bab9f2d4191d921316989aa1715ff91feb50
ankit <ankit.sanghavi87@gmail.com>
2021-11-15 15:22:41 +0530

    finished simple extension

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import './assets/css/styles.css'
 import './App.css';
+import Link from './components/Link';
+import ars from './assets/images/ars.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">Link House</h1>
+      <div className="links-container">
+        <Link
+          imgSrc={`https://s2.googleusercontent.com/s2/favicons?domain_url=${"linkedin.com"}`}
+          text={'LinkedIn'}
+          address={"https://www.linkedin.com/in/ankit-sanghvi-99237b1a0/"}
+        />
+        <Link
+          imgSrc={ars}
+          text={'Website'}
+          address={"https://ankitsanghvi.live"}
+        />
+
+      </div>
     </div>
   );
 }
---
+++ src/components/Link.tsx
@@ -0,0 +1,43 @@
+import React, { useRef } from 'react'
+import copy from '../assets/images/copy.png'
+import goto from '../assets/images/goto.png'
+
+interface LinkI {
+    text: string,
+    imgSrc: string,
+    address:string
+}
+
+const Link: React.FC<LinkI> = ({ text, imgSrc, address }) => {
+    const ref = useRef<HTMLDivElement>(null)
+
+
+    const handleClick = () => {
+        const el = ref.current!
+        navigator.clipboard.writeText(el.getAttribute('data-value')!);
+        el.classList.add('clicked')
+        setTimeout(() => {
+            el.classList.remove('clicked')
+        },1000)
+    }
+
+
+    return (
+        <div className="link-container" ref={ref} data-value={address} onClick={handleClick}  >
+            <div className="info-container">
+                <div className="img-container">
+                    <img src={imgSrc} alt="" />
+                </div>
+                <p className="link-name" >{text}</p>
+            </div>
+            <div className="action-container">
+                <img src={copy} alt="copy" onClick={handleClick} />
+                <a onClick={(e: any) => { e.stopPropagation()}} href={address} target="_blank" >
+                    <img src={goto} alt="go to"/>
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default Link
